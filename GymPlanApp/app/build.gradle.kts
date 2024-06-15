2ebfd1d98ea5ff75f2a7c229b926c6b99655bc85
Mateusz Walczyk <mattiusw@gmail.com>
2024-06-15 15:34:00 +0200

    Pobranie wszystkich zaleznosci potrzebnych do stworzenia aplikacji

--- GymPlanApp/app/build.gradle.kts
+++ GymPlanApp/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,9 +34,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    val room_version = "2.6.1"
+
+    val nav_version = "2.7.7"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // Live data
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
+
+    // View model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.2")
+
+    // Fragments
+    implementation("androidx.fragment:fragment-ktx:1.8.0")
+
+    // Kotlin Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)

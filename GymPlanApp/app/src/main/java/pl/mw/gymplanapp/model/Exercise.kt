b323e3d08bb30a5e4da38580e28d2bb527f39693
Mateusz Walczyk <mattiusw@gmail.com>
2024-06-16 11:30:18 +0200

    Dodanie id z planu treningowego

--- GymPlanApp/app/src/main/java/pl/mw/gymplanapp/model/Exercise.kt
+++ GymPlanApp/app/src/main/java/pl/mw/gymplanapp/model/Exercise.kt
@@ -1,14 +1,25 @@
 package pl.mw.gymplanapp.model
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.math.BigDecimal
-@Entity(tableName = "exercises_table")
+
+
+@Entity(tableName = "exercises_table",
+    foreignKeys = [ForeignKey(
+        entity = TrainingPlan::class,
+        parentColumns = ["id"],
+        childColumns = ["trainingPlanId"],
+        onDelete = ForeignKey.CASCADE // Jeżeli klucz w tabeli nadrzędnej zostanie usunięty to wszystkie powiązane rekordy z tabeli podrzędnej również
+    )]
+    )
 data class Exercise(
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
     val name_exercise: String,
     val amount_exercise: Int,
     val weight_exercise: BigDecimal,
-    val category_exercise: String
+    val category_exercise: String,
+    val trainingPlanId: Int
 )

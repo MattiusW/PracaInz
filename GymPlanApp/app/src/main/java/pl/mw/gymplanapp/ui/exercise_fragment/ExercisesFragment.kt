d36b94963e42b5a445d6fd279f49e7503f218860
Mateusz Walczyk <mattiusw@gmail.com>
2024-07-13 10:26:32 +0200

    Stworzenie logiki, utworzenie kopii z encji z bazy danych i zmodyfikowanie pola serii dodanie limitu od 0 do 99

--- GymPlanApp/app/src/main/java/pl/mw/gymplanapp/ui/exercise_fragment/ExercisesFragment.kt
+++ GymPlanApp/app/src/main/java/pl/mw/gymplanapp/ui/exercise_fragment/ExercisesFragment.kt
@@ -2,7 +2,6 @@ package pl.mw.gymplanapp.ui.exercise_fragment
 
 import androidx.fragment.app.viewModels
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,10 +13,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import pl.mw.gymplanapp.MainActivity
 import pl.mw.gymplanapp.MainViewModel
 import pl.mw.gymplanapp.R
+import pl.mw.gymplanapp.buttons.OnMinusSeriesClickListener
+import pl.mw.gymplanapp.buttons.OnPlusSeriesClickListener
 import pl.mw.gymplanapp.databinding.FragmentExercisesBinding
+import pl.mw.gymplanapp.model.Exercise
 import pl.mw.gymplanapp.ui.adapters.ExercisesAdapter
 
-class ExercisesFragment : Fragment() {
+class ExercisesFragment : Fragment(), OnMinusSeriesClickListener, OnPlusSeriesClickListener {
 
     private val exercisesViewModel by viewModels<ExercisesViewModel>()
     private val mainVm by activityViewModels<MainViewModel>()
@@ -51,7 +53,7 @@ class ExercisesFragment : Fragment() {
         mainVm.getSelectedTrainingPlan()?.let { trainingPlan ->
             mainVm.getAllExercisesByPlan(trainingPlan.planId).observe(viewLifecycleOwner) {
                 exercises ->
-                binding.exerciseRecylerView.adapter = ExercisesAdapter(exercises) {
+                binding.exerciseRecylerView.adapter = ExercisesAdapter(exercises,this, this) {
                     exercises, position ->
                     mainVm.selectExercise(exercises)
                     findNavController().navigate(R.id.editExerciseFragment)
@@ -68,4 +70,16 @@ class ExercisesFragment : Fragment() {
     private fun handleOnBackPressed() {
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
     }
+
+    // Zrobienie kopii encji cwiczenie oraz update zmniejszenie pola o 1
+    override fun onMinusSeriesClick(exercise: Exercise) {
+        val updateMinusSeries = exercise.copy(amount_exercise = (exercise.amount_exercise - 1).coerceAtLeast(0))
+        mainVm.updateExercise(updateMinusSeries)
+    }
+
+    override fun OnPlusSeriesClick(exercise: Exercise) {
+        val updatePlusSeries = exercise.copy(amount_exercise = (exercise.amount_exercise + 1).coerceAtMost(99))
+        mainVm.updateExercise(updatePlusSeries)
+    }
+
 }

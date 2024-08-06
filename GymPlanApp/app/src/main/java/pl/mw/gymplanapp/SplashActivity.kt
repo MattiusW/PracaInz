5f0192728da8a38ed5b568d813fe05d33d23baf2
Mateusz Walczyk <mattiusw@gmail.com>
2024-08-06 09:54:57 +0200

    Dodanie loopera do przestarzalej wersji Handler ktory zwieksza jego jakosc

--- GymPlanApp/app/src/main/java/pl/mw/gymplanapp/SplashActivity.kt
+++ GymPlanApp/app/src/main/java/pl/mw/gymplanapp/SplashActivity.kt
@@ -3,6 +3,7 @@ package pl.mw.gymplanapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,10 +15,10 @@ class SplashActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 3000L)
     }
 }
